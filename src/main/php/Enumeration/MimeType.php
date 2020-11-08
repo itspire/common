cdd027601a7a9fdd79adc9e9a983bfdd17c8ddec
Ryan RAJKOMAR <ryan.rajkomar@itspire.fr>
2020-11-08 17:40:22 +0100

    Disabled phpcs rule for Line Length in MimeType.php : file is auto generated

--- src/main/php/Enumeration/MimeType.php
+++ src/main/php/Enumeration/MimeType.php
@@ -4,6 +4,8 @@
  * Copyright (c) 2016 - 2020 Itspire.
  * This software is licensed under the BSD-3-Clause license. (see LICENSE.md for full license)
  * All Right Reserved.
+ *
+ * @phpcs:disable Generic.Files.LineLength.TooLong
  */
 
 declare(strict_types=1);
